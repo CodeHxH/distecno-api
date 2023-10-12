85e87016531fffe4aceb50051dbc36f6415624f8
CodeHxH <rodriguezjoseagustin31@gmail.com>
2023-10-12 12:54:25 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import authRoutes from './routes/auth.routes.js';
+import tasksRoutes from './routes/task.routes.js';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api', authRoutes);
+app.use('/api', tasksRoutes);
+
+export default app;
