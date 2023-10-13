e132241e8397e749175b2a64bb7e06840b95f107
HoriiSan <nitto.ve@gmail.com>
2023-10-13 13:39:25 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -14,12 +14,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
-app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        credentials: true,
-    })
-);
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
